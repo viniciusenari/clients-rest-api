cca4ae00681c00c5429421076ea3207838fd8ab4
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-10-26 09:59:48 -0300

    feat: add pagination

--- setup/settings.py
+++ setup/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Pagination
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
