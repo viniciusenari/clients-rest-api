a2265aee73d826f087cc28c80527d0f241832400
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-10-26 09:38:56 -0300

    refactor: change ssn length to include "-"

--- clients/models.py
+++ clients/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(blank=False, max_length=30, )
-    ssn = models.CharField(max_length=9, unique=True)
+    ssn = models.CharField(max_length=11, unique=True)
     phone = models.CharField(max_length=15)
     active = models.BooleanField()
 
