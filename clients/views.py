88bb6529a70f284f5ee99a4a550e74670e600ca2
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-10-26 15:24:04 -0300

    feat: include authentication

--- clients/views.py
+++ clients/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, filters
 from clients.serializers import ClientSerializer
 from clients.models import Client
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ClientsViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
@@ -10,3 +12,5 @@ class ClientsViewSet(viewsets.ModelViewSet):
     ordering_fields = ['name']
     search_fields = ['name', 'ssn']
     filterset_fields = ['active']
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
